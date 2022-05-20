4f250c8f6d3cc9210b72e3b34b67f3e3c936f075
Jules Stoltz <julesstoltz@gmail.com>
2022-05-20 19:30:34 -0400

    Add button behavior to change the button label, toggle the turn, and disable the button from selection

--- tiktak/tik.py
+++ tiktak/tik.py
@@ -3,15 +3,28 @@ kivy.require('2.1.0')
 
 from kivy.app import App
 from kivy.lang.builder import Builder
+from kivy.app import StringProperty
 
 kv = Builder.load_file('tik.kv')
 
 
 class MainApp(App):
-    
+    turn = StringProperty()
+
     def build(self):
+        self.turn = 'X'
         return kv
 
+    def toggleTurn(self):
+        match self.turn:
+            case 'X':
+                self.turn = 'O'
+                return "X"
+            case 'O':
+                self.turn = 'X'
+                return 'O'
+            case _:
+                return ''
 
 if __name__ == '__main__':
     MainApp().run()
